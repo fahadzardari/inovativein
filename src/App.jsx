0a92b40689c09aeef2ed238c266bff27fc7589d1
fahad <fahadzardari111@gmail.com>
2023-05-05 03:03:07 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import Home from "./Sections/Home"
+export default function App() {
+  return (
+    <div className="container ">
+        <Home />
+    </div>  
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      homePageButtonBlack: '#292930',
+      homePageServicesPurple: '#5956E8',
+      // ...
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
