5e3edb8f65b04f21eb9358a4e0b98b3d44321049
arayanmemon <arayanmemon338@gmail.com>
2023-05-08 00:00:59 +0500

    Added Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Home from "./Sections/Home"
 import Services from "./Sections/Services"
+import Footer from "./Sections/Footer"
 export default function App() {
   return (
     <div className="font-nunito w-full m-0 p-0 ">
         <Home />
         <Services />
+        <Footer />
     </div>  
   )
 }
