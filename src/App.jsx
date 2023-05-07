8c18b90145366c8fef07a36d5543d235aa29582f
fahad <fahadzardari111@gmail.com>
2023-05-07 18:02:43 +0500

    2nd section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Home from "./Sections/Home"
+import Services from "./Sections/Services"
 export default function App() {
   return (
-    <div className="font-nunito container m-0 p-0">
+    <div className="font-nunito container m-0 p-0 ">
         <Home />
+        <Services />
     </div>  
   )
 }
---
+++ src/Sections/Services.jsx
@@ -0,0 +1,8 @@
+
+export default function Services() {
+  return (
+    <div className="min-h-screen min-w-[100vw] m-0 p-0 bg-homepageprimary">
+sdfkl
+    </div>
+  )
+}
