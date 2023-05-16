b06aaab2d2ce9a622224f33c13d75b971471e9d7
arayanmemon <arayanmemon338@gmail.com>
2023-05-16 10:55:24 +0500

    Made Footer responsive

--- src/Sections/Footer.jsx
+++ src/Sections/Footer.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 export default function Footer() {
   return (
     <div className='w-full'>
-      <div className='bg-gray1 w-[90%] mx-auto h-[90vh] py-10'>
-        <div className='flex justify-around'>
+      <div className='bg-gray1 sm:w-[90%] sm:mx-auto sm:h-[34rem] py-10'>
+        <div className='flex items-center sm:justify-around sm:flex-row flex-col'>
           <div className='h-[29rem] w-[18rem] bg-gray2 space-y-4 px-6 py-10'>
             <h3 className='text-white text-xl'>InovativeIn</h3>
             <img src="/homepage-section/logo.png" className='mx-auto' alt="" />
@@ -14,13 +14,13 @@ export default function Footer() {
             <button className='bg-yellow-500 h-[3rem] w-[14rem] rounded-lg'>Subscribe</button>
           </div>
 
-          <div className='w-1/2 text-white'>
+          <div className='w-[90%] sm:w-1/2 text-white'>
             <div className='flex justify-between py-8'>
               <div className=''>
-                <p>Get in touch with</p><p>+92 312-456789</p>
+                <p>Get in touch with us</p><p>+92 312-456789</p>
               </div>
               <div>
-                <p>Location</p><p>phase 1 society Jamshoro</p>
+                <p>Location:</p><p>phase 1 society Jamshoro</p>
               </div>
             </div>
             {/* Upper footer end*/}
@@ -28,33 +28,33 @@ export default function Footer() {
 
             <div className='pt-12 pb-6 flex justify-between'>
                 <div>
-                  <h3>Pages</h3>
+                  <h3 className='text-xl'>Pages</h3>
                   <ul className='pt-6 space-y-2'>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Blog</li>
-                    <li>Pricing</li>
-                    <li>Contact</li>
-                    <li>Services</li>
+                    <li><a href='#'  className='hover:text-lime-300 hover:delay-300'>Home</a></li>
+                    <li><a href='#'  className='hover:text-lime-300 hover:delay-300'>About</a></li>
+                    <li><a href='#'  className='hover:text-lime-300 hover:delay-300'>Blog</a></li>
+                    <li><a href='#'  className='hover:text-lime-300 hover:delay-300'>Pricing</a></li>
+                    <li><a href='#'  className='hover:text-lime-300 hover:delay-300'>Contact</a></li>
+                    <li><a href='#'  className='hover:text-lime-300 hover:delay-300'>Services</a></li>
                   </ul>
                 </div>
                 <div>
-                <h3>Utlility Pages</h3>
+                <h3 className='text-xl'>Utlility Pages</h3>
                   <ul className='pt-6 space-y-2'>
-                    <li>Styleguide</li>
-                    <li>404 Not Found</li>
-                    <li>Password protected</li>
-                    <li>Licences</li>
-                    <li>Changelog</li>
+                    <li><a href='#'  className='hover:text-lime-300 hover:delay-300'>Styleguide</a></li>
+                    <li><a href='#'  className='hover:text-lime-300 hover:delay-300'>404 Not Found</a></li>
+                    <li><a href='#'  className='hover:text-lime-300 hover:delay-300'>Password protected</a></li>
+                    <li><a href='#'  className='hover:text-lime-300 hover:delay-300'>Licences</a></li>
+                    <li><a href='#'  className='hover:text-lime-300 hover:delay-300'>Changelog</a></li>
                   </ul>
                 </div>
                 <div>
-                <h3>Follows</h3>
+                <h3 className='text-xl'>Follows</h3>
                   <ul className='pt-6 space-y-2'>
-                    <li>Facebook</li>
-                    <li>Twitter</li>
-                    <li>Instagram</li>
-                    <li>Linkedin</li>
+                    <li><a href='#'  className='hover:text-lime-300 hover:delay-300'>Facebook</a></li>
+                    <li><a href='#'  className='hover:text-lime-300 hover:delay-300'>Twitter</a></li>
+                    <li><a href='#'  className='hover:text-lime-300 hover:delay-300'>Instagram</a></li>
+                    <li><a href='#'  className='hover:text-lime-300 hover:delay-300'>Linkedin</a></li>
                   </ul>
                 </div>
             </div>
