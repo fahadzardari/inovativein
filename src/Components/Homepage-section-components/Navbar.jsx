ac840e86c95afe707f446a4b64a61f80491d764d
fahad <fahadzardari111@gmail.com>
2023-08-13 16:17:20 +0500

    mantine added + dashboard prelim set

--- src/Components/Homepage-section-components/Navbar.jsx
+++ src/Components/Homepage-section-components/Navbar.jsx
@@ -1,34 +1,123 @@
+import {
+  Burger,
+  Header,
+  Container,
+  Group,
+  Transition,
+  Paper,
+} from "@mantine/core";
+import { useState } from "react";
+
+const classes = {
+  dropdown: "absolute z-50 top-8 right-0 mt-16 w-full bg-white shad",
+  dropdownInner: "py-8 px-10 flex flex-col gap-4",
+};
+
 export default function Navbar() {
+  const [opened, setOpened] = useState(false);
+
   return (
-    <nav className="flex grow-1 items-center  text-homepageprimary justify-between">
-      <div className="flex flex-col items-center">
-        <img src="/homepage-section/logo.png" alt="" />
-        <h1 className="text-2xl font-bold text-[#2C2D36]">Inovativein</h1>
-      </div>
-
-      <ul className="flex items-center gap-16  text-lg grow-1 ">
-        <li className="">
-        <a className="" href="#">Home</a>
-        </li>
-        <li className="">
-        <a className="" href="#">About</a>
-        </li>
-        <li className="">
-        <a className="" href="#">Services</a>
-        </li>
-        <li className="">
-        <a className="" href="#">Pages</a>
-        </li>
-        <li className="">
-        <a className="" href="#">Login</a>
-        </li>
-
-        <li>
-          <button className="bg-homepageprimary text-white px-8 py-4 rounded-lg">
+    <Header className="bg-transparent border-0">
+      <Container className="flex justify-between items-center w-full py-4">
+        <div className="flex-col justify-center items-center text-center">
+          <img src="/homepage-section/logo.png" className="" alt="logo" />
+          <h1 className="text-xs md:text-2xl font-bold text-[#2C2D36]">Inovativein</h1>
+        </div>
+        <Group gap={5} className="hidden md:flex md:text-lg">
+          <a href="">Home</a>
+          <a href="">About</a>
+          <a href="">Services</a>
+          <a href="">Login</a>
+          <a href=""
+            className="bg-homepageprimary text-white px-8 py-4 rounded-lg"
+          >
             Get Started
-          </button>
-        </li>
-      </ul>
-    </nav>
+          </a>
+        </Group>
+
+        <Burger
+          opened={opened}
+          onClick={() => setOpened(!opened)}
+          className="md:hidden"
+        />
+        <Transition
+          mounted={opened}
+          transition="fade"
+          duration={200}
+          timingFunction="ease"
+        >
+          {(styles) => (
+            <Paper className={classes.dropdown} withBorder style={styles}>
+              <Group position="apart" className={classes.dropdownInner}>
+                <a href="">Home</a>
+                <a href="">About</a>
+                <a href="">Services</a>
+                <a href="">Login</a>
+              </Group>
+            </Paper>
+          )}
+        </Transition>
+      </Container>
+    </Header>
   );
 }
+
+// import {
+//   Burger,
+//   Header,
+//   Container,
+//   Group,
+//   Transition,
+//   Paper,
+// } from "@mantine/core";
+// import { useState } from "react";
+// export default function Navbar() {
+//   const [opened, setOpened] = useState(false);
+//   return (
+//     <Header className="bg-transparent w-full border-0">
+//       <Container className="flex justify-between items-center w-full">
+//         <img src="/homepage-section/logo.png" className="h-14" alt="" />
+//         <Group gap={5} className="hidden md:flex">
+//           <a href="">Home</a>
+//           <a href="">About</a>
+//           <a href="">Services</a>
+//           <a href="">Login</a>
+//         </Group>
+
+//         <Burger className="md:hidden" opened={opened} onClick={() => setOpened(!opened)} />
+
+//       </Container>
+//     </Header>
+//     // <nav className="flex grow-1 items-center  text-homepageprimary justify-between mx-4 py-8">
+//     //   {/* <div className="flex flex-col items-center"> */}
+//     //     <img src="/homepage-section/logo.png" alt="logo" className="h-8" />
+//     //     {/* <h1 className="text-xs md:text-2xl text-[#2C2D36]">Inovativein</h1> */}
+//     //   {/* </div> */}
+
+//     //   <ul className="hidden md:flex items-center gap-16 text-lg grow-1">
+//     //     <li className="">
+//     //     <a className="" href="#">Home</a>
+//     //     </li>
+//     //     <li className="">
+//     //     <a className="" href="#">About</a>
+//     //     </li>
+//     //     <li className="">
+//     //     <a className="" href="#">Services</a>
+//     //     </li>
+//     //     <li className="">
+//     //     <a className="" href="#">Pages</a>
+//     //     </li>
+//     //     <li className="">
+//     //     <a className="" href="#">Login</a>
+//     //     </li>
+
+//     //     <li>
+//     //       <button className="bg-homepageprimary text-white px-8 py-4 rounded-lg">
+//     //         Get Started
+//     //       </button>
+//     //     </li>
+//     //   </ul>
+
+//     // </nav>
+//   );
+// }
--- src/Sections/Home.jsx
+++ src/Sections/Home.jsx
@@ -2,29 +2,47 @@ import Navbar from "../Components/Homepage-section-components/Navbar";
 export default function Home() {
   return (
     <div
-      style={{ backgroundImage: "url(/homepage-section/bg.png" , backgroundRepeat:"no-repeat" }}
-      className="min-h-screen w-full  "
+      style={{
+        backgroundImage: "url(/homepage-section/bg.png",
+        backgroundRepeat: "no-repeat",
+      }}
+      className="min-h-screen"
     >
-    
-      <div className="container relative mx-auto  px-[8rem] pt-8">
-        <img src="/homepage-section/marketing-banner.png" className="invisible lg:visible absolute w-[55rem] h-[50rem] -right-20 lg-right-10 top-40 " />
-        <Navbar />
-        <div className="content w-3/6 mt-12">
-          <h1 className="font-extrabold text-[4rem]">
-            A fully integrated digital{" "}
-            <span className="text-homepageservices">Services</span> agency
-          </h1>
-          <p className="text-[#555555] sm:text-sm md:text-md lg:text-lg mt-10">
-            Lorem ipsum dolor sit amet, ea mundi malorum cum, ocurreret
-            reprehendunt ius ex. Ut vis mazim erroribus forensibus
-          </p>
-          <div className="flex mt-8 gap-4">
-          <button className="text-white bg-homepageprimary hover:bg-transparent hover:text-homepageprimary  border hover:border-homepageprimary font-light px-8 py-6 rounded-lg">
-              Our Services
-            </button>
-            <button className="text-white bg-homepageprimary hover:bg-transparent hover:text-homepageprimary  border hover:border-homepageprimary font-light px-8 py-6 rounded-lg">
-              Getting Started
-            </button>
+      <Navbar />
+      <div className="flex flex-col justify-center items-center lg:mt-12 lg:flex-row lg:px-36">
+        <div className="flex items-center justify-center w-full order-2 px-10 lg:px-0 lg:order-1">
+          <div className="max-w-2xl mb-8">
+            <h1 className="text-xl text-left lg:text-left font-bold leading-snug tracking-tight lg:text-6xl lg:leading-tight">
+              A fully integrated digital{" "}
+              <span className="text-homepageservices">Services</span> agency
+            </h1>
+            <p className="py-5 text-sm leading-normal lg:text-xl">
+              Lorem ipsum dolor sit amet, ea mundi malorum cum, ocurreret
+              reprehendunt ius ex. Ut vis mazim erroribus forensibus
+            </p>
+            <div className="flex flex-col items-center space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
+              <a
+                href="https://web3templates.com/templates/nextly-landing-page-template-for-startups"
+                rel="noopener"
+                className="px-8 py-4 text-xs w-2/3 lg:w-1/3 font-medium text-center text-white bg-black hover:bg-transparent border border-black hover:text-black rounded-md "
+              >
+                Our Services
+              </a>
+              <a
+                href="https://web3templates.com/templates/nextly-landing-page-template-for-startups"
+                rel="noopener"
+                className="px-8 py-4 text-xs w-2/3 lg:w-1/3 font-medium text-center text-black bg-transparent hover:bg-black border border-black hover:text-white rounded-md "
+              >
+                Get Started
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="flex order-1 lg:order-2 items-center justify-center w-full">
+          <div className="">
+            <img
+              src="/homepage-section/marketing-banner.png"
+            />
           </div>
         </div>
       </div>
